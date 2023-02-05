cd084d6ac83040af0a2bd1b729c97e1fe5675863
Prince-Will01 <princewillonwuakpa@gmail.com>
2023-02-05 16:37:11 +0100

    py

---
+++ guess-the-word.py
@@ -0,0 +1,115 @@
+import random
+import time
+
+import speech_recognition as sr
+
+
+def recognize_speech_from_mic(recognizer, microphone):
+    """Transcribe speech from recorded from `microphone`.
+
+    Returns a dictionary with three keys:
+    "success": a boolean indicating whether or not the API request was
+               successful
+    "error":   `None` if no error occured, otherwise a string containing
+               an error message if the API could not be reached or
+               speech was unrecognizable
+    "transcription": `None` if speech could not be transcribed,
+               otherwise a string containing the transcribed text
+    """
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    # try considering the speech in the recording to be valid
+    # if a RequestError or UnknownValueError exception is noticed,
+    #     modify the response object accordingly
+    try:
+        response["transcription"] = recognizer.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
+
+
+if __name__ == "__main__":
+    # set the list of words, maximum number of guesses, and prompt limit
+    WORDS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "August", "September", "October", "November", "December"] 
+    NUM_GUESSES = 3
+    PROMPT_LIMIT = 5
+
+    # formulate recognizer and microphone instances
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+
+    # select a random word from the list
+    word = random.choice(WORDS)
+
+    # format the instructions string
+    instructions = (
+        "I'm thinking of one of these words:\n"
+        "{words}\n"
+        "You have {n} tries to guess which one.\n"
+    ).format(words=', '.join(WORDS), n=NUM_GUESSES)
+
+    # show instructions and wait 3 seconds before starting the game
+    print(instructions)
+    time.sleep(3)
+
+    for i in range(NUM_GUESSES):
+        # get the guess from the user
+        # if a transcription is returned, break out of the loop and
+        #     continue
+        # if no transcription returned and API request failed, break
+        #     loop and continue
+        # if API request succeeded but no transcription was returned,
+        #     re-prompt the user to say their guess again. Do this up
+        #     to PROMPT_LIMIT times
+        for j in range(PROMPT_LIMIT):
+            print('Guess {}. Speak!'.format(i+1))
+            guess = recognize_speech_from_mic(recognizer, microphone)
+            if guess["transcription"]:
+                break
+            if not guess["success"]:
+                break
+            print("Come on...... What did you say?\n")
+
+        # if there was an error, halt the game
+        if guess["error"]:
+            print("ERROR: {}".format(guess["error"]))
+            break
+
+        # show the user the transcription
+        print("You said: {}".format(guess["transcription"]))
+
+        # determine if guess is valid and if any attempts remain
+        guess_is_correct = guess["transcription"].lower() == word.lower()
+        user_has_more_attempts = i < NUM_GUESSES - 1
+
+        # determine if the user has won the game
+        # if not, repeat the loop if user has more attempts
+        # if no attempts left, the user loses the game
+        if guess_is_correct:
+            print("Correct! You win!".format(word))
+            break
+        elif user_has_more_attempts:
+            print("Incorrect. Try again.\n")
+        else:
+            print("Sorry, you lose!\nI was thinking of '{}'.".format(word))
+            break
